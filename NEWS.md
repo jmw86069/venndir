2d50b369ad84af1db1f59d7f487b2b5aa221c0a2
jmw86069 <jmw86069@gmail.com>
2020-10-02 19:25:08 -0400

    Added textvenn() for console text output.

---
+++ NEWS.md
@@ -0,0 +1,42 @@
+# venndir version 0.0.2.900
+
+The `venndir` package is intended for analyses where direction
+is encoded alongside each item in a set. The driving example
+is for gene expression analysis, which results in a subset
+of differentially expressed genes, where the expression can
+either by "up-regulation" or "down-regulation." When comparing
+two sets of differentially expressed genes, it is helpful
+if not essential to recognize when any overlaps also share
+the same direction of change (or opposite direction.)
+
+Although there are some 40,000 gene loci in human (give or take),
+it is common to see many of the same few hundred genes
+differentially regulated across a very diverse array of
+experiments. For two experiments to identify 800 genes each,
+with overlap of 400, these outcomes illustrate the utility
+of venndir:
+
+* 400 genes overlap, 200 share the same direction, 200 are opposite.
+In this case, the overlap may represent response by similar functional
+set of genes, but with different biological drivers.
+* 400 genes overlap, 396 genes share the same direction. In this case,
+the overlap could be seen as highly similar between the two sets.
+* 400 genes overlap, only 4 genes share the same direction. In this
+case, the overlap could be see as nearly exactly opposite.
+
+## new functions
+
+* `textvenn()` prints a Venn diagram using console text output.
+It also supports directionality, the formatting is still being
+adjusted for best visual appeal.
+* `signed_overlaps()` is the core overlap function for venndir,
+it performs directional and direct overlap, with configurable
+output. It returns a `data.frame` with counts for each Venn overlap
+set, and optionally includes items. The exact output format may
+be adjusted in future, currently it returns one row per overlap.
+For directional overlaps, it returns one row per combination of
+up/down.
+* `make_venn_test()` is a convenience function to generate test
+sets for Venn diagrams. It can return simple sets, or sets with
+directionality. It is mainly intended to help create very large
+sets to test efficiency of the process.
--- R/venndir-textvenn.R
+++ R/venndir-textvenn.R
@@ -7,16 +7,54 @@
 #' using text, intended to be displayed using mono-spaced font
 #' on an R console.
 #' 
+#' @return `data.frame` returned using `invisible()`, from the
+#'    output of `signed_overlaps()`.
+#' 
+#' @param setlist `list` of item vectors; `list` of vectors named by item;
+#'    incidence `matrix` with values `c(0, 1)` or `c(FALSE, TRUE)`, or
+#'    `c(-1, 0, 1)`.
+#' @param sets `integer` `vector` as index to `setlist`, used to
+#'    pull out a subset of the list elements. This subset is useful
+#'    because the set colors are defined for the full `setlist`,
+#'    which allows the subset of colors to be consistent for each set.
+#' @param set_colors `NULL` or `character` `vector` that contains
+#'    R-compatible colors. When `set_colors` is `NULL`, categorical
+#'    colors are defined using `colorjam::rainbowJam()`. When
+#'    `set_colors` is defined, the values are recycled to the total
+#'    number of sets represented by `setlist`.
+#' @param spacing,padding `integer` values indicating the character
+#'    spacing and padding around labels printed to the console output.
+#' @param inverse_title,inverse_counts `logical` indicating whether
+#'    to inverse the color, when `inverse_title=TRUE` then each Venn
+#'    set is printed on colored background, when `inverse_title=FALSE`,
+#'    each set is printed with colored text with no background color.
+#' @param return_items `logical` passed to `signed_overlaps()` to
+#'    indicate whether to include items with each Venn set. This data
+#'    is returned using `invisible()`, and is relevant only when
+#'    storing the return value.
+#' @param verbose `logical` indicating whether to print verbose output.
+#' 
+#' @examples
+#' setlist <- make_venn_test(n_items=100, do_signed=TRUE)
+#' textvenn(setlist, sets=c(1,2))
+#' 
+#' setlist <- make_venn_test(n_items=100000, do_signed=TRUE)
+#' textvenn(setlist, sets=c(1,2,3))
+#' signed_overlaps(setlist, "each")
+#' 
 #' @export
 textvenn <- function
 (setlist,
  sets=seq_along(setlist),
+ overlap_type=c("overlap", "each", "concordance", "concordant"),
  set_colors=NULL,
  spacing=5,
  padding=2,
- useBgColor=TRUE,
- countsUseBgColor=FALSE,
+ inverse_title=TRUE,
+ inverse_counts=FALSE,
+ color_by_counts=TRUE,
  return_items=FALSE,
+ unicode=TRUE,
  verbose=FALSE,
  ...)
 {
@@ -24,6 +62,7 @@ textvenn <- function
    if (n > 3) {
       stop("The textvenn() function currently only supports 2 or 3 sets.");
    }
+   overlap_type <- match.arg(overlap_type);
    
    # define colors
    if (length(set_colors) == 0) {
@@ -35,16 +74,49 @@ textvenn <- function
          length.out=n);
    }
 
+   #c("^(-1.* 1.*|1.* -1.*)$", "\u21C6"),
+   if (unicode) {
+      curate_list <- list(
+         c("[ ]*-1", "\u2193"),
+         c("[ ]*1", "\u2191"),
+         c("[ ]*concordant", "\u21F6"),
+         c("[ ]*mixed", "\u21C6"));
+   } else {
+      curate_list <- list(
+         c("[ ]*-1", "v"),
+         c("[ ]*1", "^"),
+         c("[ ]*concordant", ">>>"),
+         c("[ ]*mixed", ">|<"));
+   }
+   curate_df <- jamba::rbindList(curate_list)
+   curate_labels <- function(x, curate_df){
+      for (i in seq_len(nrow(curate_df))) {
+         x <- gsub(curate_df[i,1], curate_df[i,2], x)
+      }
+      x;
+   }
+
    # get overlap data
    sv <- signed_overlaps(setlist[sets],
-      "overlap",
+      overlap_type=overlap_type,
       return_items=return_items);
    #sv <- signed_overlaps(list(A=letters[1:10], B=LETTERS[1:20]), "overlap");
    #sv
    
-   fCounts <- format(sv$count,
-      big.mark=",",
-      trim=TRUE);
+   # numeric counts
+   nCounts <- sapply(unique(sv$sets), function(i){
+      sum(subset(sv, sets %in% i)$count)
+   });
+   # formatted numeric counts
+   fCounts <- format(big.mark=",",
+      trim=TRUE,
+      nCounts);
+   # grouped counts (directional)
+   gCounts <- lapply(jamba::nameVector(unique(sv$sets)), function(i){
+      j <- subset(sv, sets %in% i);
+      jamba::nameVector(j$count, j$overlap_label)
+   });
+   
    ctNchar <- max(nchar(fCounts));
    nameNchar <- nchar(names(setlist));
    spacer <- paste(rep(" ", length.out=spacing), collapse="");
@@ -52,32 +124,40 @@ textvenn <- function
    if (n == 2) {
       ## 2-way Venn, one central number
       vCol <- set_color;
-      rad2deg <- jamba::rad2deg;
       vCol12 <- colorjam::blend_colors(vCol);
       
       ## Create matrix for labels
-      set_colnums <- c(1,5,3);
-      venn_m <- matrix(ncol=5, nrow=2, data=" ");
+      set_colnums <- c(1,5,3)*2 - 1;
+      venn_m <- matrix(ncol=10, nrow=2, data=" ");
       venn_m[1,set_colnums] <- sv$sets;
-      venn_m[2,set_colnums] <- fCounts;
+      venn_m[2,set_colnums] <- nCounts;
       
       # matrix for label colors
-      count_colors <- colorjam::vals2colorLevels(sqrt(sv$count),
-         divergent=FALSE,
-         col="Reds",
-         trimRamp=c(8, 1),
-         lens=2,
-         baseline=0);
-      venn_c <- matrix(ncol=5, nrow=2, data=NA);
-      venn_c[1,set_colnums] <- c(vCol, vCol12);
+      header_colors <- c(vCol, vCol12);
+      if (color_by_counts) {
+         count_colors <- colorjam::vals2colorLevels(sqrt(nCounts),
+            divergent=FALSE,
+            col="Reds",
+            trimRamp=c(8, 1),
+            lens=2,
+            baseline=0);
+      } else {
+         if (inverse_counts) {
+            count_colors <- header_colors;
+         } else {
+            count_colors <- c("darkorange3");
+         }
+      }
+      venn_c <- matrix(ncol=10, nrow=2, data=NA);
+      venn_c[1,set_colnums] <- header_colors;
       venn_c[2,set_colnums] <- count_colors;
-      
+
       # matrix indicating what colors to invert
-      venn_i <- matrix(ncol=5, nrow=2, data=FALSE);
-      if (useBgColor) {
+      venn_i <- matrix(ncol=10, nrow=2, data=FALSE);
+      if (inverse_title) {
          venn_i[1,set_colnums] <- TRUE;
       }
-      if (countsUseBgColor) {
+      if (inverse_counts) {
          venn_i[2,set_colnums] <- TRUE;
       }
       
@@ -95,38 +175,90 @@ textvenn <- function
       vCol23 <- colorjam::blend_colors(vCol[2:3]);
       vCol13 <- colorjam::blend_colors(vCol[c(1,3)]);
       vCol123 <- colorjam::blend_colors(vCol[c(1:3)]);
-      counts <- jamba::nameVector(sv$count, sv$sets);
-      
+
       ## Create matrix for labels
-      set_colnums <- c(1,5,3, 3,2,4, 3);
-      #set_rownums <- c(1,1,11, 3,8,8, 6);
+      set_colnums <- c(1,5,3, 3,2,4, 3) * 2 - 1;
       set_rownums <- c(3,3,12, 1,8,8, 6);
-      venn_m <- matrix(ncol=5, nrow=13, data=" ");
-      venn_m[cbind(set_rownums, set_colnums)] <- sv$sets;
-      venn_m[cbind(set_rownums + 1, set_colnums)] <- format(counts,
-         big.mark=",",
-         trim=TRUE);
+      venn_m <- matrix(ncol=10, nrow=13, data=" ");
+      venn_m[cbind(set_rownums, set_colnums)] <- names(fCounts);
+      venn_m[cbind(set_rownums + 1, set_colnums)] <- fCounts;
 
       # matrix for label colors
-      count_colors <- colorjam::vals2colorLevels(sqrt(counts),
-         divergent=FALSE,
-         col="Reds",
-         trimRamp=c(8, 1),
-         lens=2,
-         baseline=0);
       header_colors <- c(vCol, vCol12, vCol13, vCol23, vCol123);
-      venn_c <- matrix(ncol=5, nrow=13, data=NA);
+      if (color_by_counts) {
+         count_colors <- colorjam::vals2colorLevels(sqrt(nCounts),
+            divergent=FALSE,
+            col="Reds",
+            trimRamp=c(8, 1),
+            lens=2,
+            baseline=0);
+      } else {
+         if (inverse_counts) {
+            count_colors <- header_colors;
+         } else {
+            count_colors <- "darkorange3";
+         }
+      }
+      venn_c <- matrix(ncol=10, nrow=13, data=NA);
       venn_c[cbind(set_rownums, set_colnums)] <- header_colors;
       venn_c[cbind(set_rownums + 1, set_colnums)] <- count_colors;
 
       # matrix indicating what colors to invert
-      venn_i <- matrix(ncol=5, nrow=13, data=FALSE);
-      if (useBgColor) {
+      venn_i <- matrix(ncol=10, nrow=13, data=FALSE);
+      if (inverse_title) {
          venn_i[cbind(set_rownums, set_colnums)] <- TRUE;
       }
-      if (countsUseBgColor) {
+      if (inverse_counts) {
          venn_i[cbind(set_rownums + 1, set_colnums)] <- TRUE;
       }
+      
+      ## signed counts
+      if (any(lengths(gCounts) > 1)) {
+         if (color_by_counts) {
+            gcount_colors <- colorjam::vals2colorLevels(sqrt(unlist(gCounts)),
+               divergent=FALSE,
+               col="Reds",
+               trimRamp=c(8, 1),
+               lens=2,
+               baseline=0);
+         } else {
+            if (inverse_counts) {
+               gcount_colors <- rep(header_colors, lengths(gCounts));
+            } else {
+               gcount_colors <- "darkorange3";
+            }
+         }
+         seq_colnums <- rep(set_colnums + 1, lengths(gCounts));
+         seq_rownums <- rep(set_rownums + 1, lengths(gCounts)) +
+            unlist(lapply(gCounts, seq_along)) - 2;
+         gbase_labels <- curate_labels(
+            names(unlist(unname(gCounts))),
+            curate_df);
+         gcount_labels <- sapply(seq_along(unlist(gCounts)), function(i){
+            ilabel <- paste0(
+               gbase_labels[i],
+               ": ",
+               format(trim=TRUE,
+                  big.mark=",",
+                  unlist(gCounts)[i]));
+         });
+         ## order labels again?
+         gdf <- mixedSortDF(data.frame(
+            group=rep(seq_along(gCounts), lengths(gCounts)),
+            label=gbase_labels,
+            index=seq_along(gbase_labels)), byCols=c(1, 2))
+         gbase_labels <- gbase_labels[gdf$index];
+         gcount_labels <- gcount_labels[gdf$index];
+         for (i in seq_along(gcount_labels)) {
+            venn_m[seq_rownums[i],seq_colnums[i]] <- gcount_labels[[i]];
+            venn_c[seq_rownums[i],seq_colnums[i]] <- gcount_colors[[i]];
+            if (inverse_counts) {
+               venn_i[row_seq[j],set_colnum] <- TRUE;
+            }
+         }
+      }
+      
+      ## print color data.frame
       print_color_df(venn_m,
          venn_c,
          venn_i,
